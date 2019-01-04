932cc96cf9a75c21ce686d56cfdc1c2d394fa538
matrixlt <1322386221@qq.com>
2019-01-04 21:28:32 +0800

    add solveunis

---
+++ MineSweeper/SolveUnit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    class SolveUnit
+    {
+        public int mine_count;
+        HashSet<Tuple<int, int>>  blocks = new HashSet<Tuple<int, int>>{};
+
+        public SolveUnit(int count)
+        {
+            mine_count = count;
+        }
+
+        public HashSet<Tuple<int, int>> Blocks { get => blocks;}
+
+        public void AddTuple(int x, int y)
+        {
+            blocks.Add(new Tuple<int, int>(x,y));
+        }
+
+    }
+}
